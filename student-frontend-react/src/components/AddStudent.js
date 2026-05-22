b79f7df5657bb2f65fb7ecf87b02e48cf9ddfa0b
houssem-dev-sh <houssembenkhemmou@gmail.com>
2026-05-22 08:32:32 +0200

    Update AddStudent.js

--- student-frontend-react/src/components/AddStudent.js
+++ student-frontend-react/src/components/AddStudent.js
@@ -5,7 +5,7 @@ function AddStudent({ refresh }) {
   const [last, setLast] = useState("");
 
   const add = () => {
-    fetch("http://localhost:8089/student/api/students", {
+    fetch("/student/api/students", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
